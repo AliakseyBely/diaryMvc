e6829563af7b71a37a2bab62b49d808ca63692bf
AliakseyBely <Ellectro@tut.by>
2017-06-19 02:09:32 +0300

    Проработаны кнопки смены фона страницы, подключена jQuery

---
+++ WebApplication1/Scripts/SiteScript.js
@@ -0,0 +1,18 @@
+﻿$(document).ready(function () {
+    
+    $("#orang").on("click", function () {
+        $("body").css({ backgroundColor: "#fed0af" });
+    });
+
+    $("#red").on("click", function () {
+        $("body").css({ backgroundColor: "#fe9e9e" });
+    });
+
+    $("#blue").on("click", function () {
+        $("body").css({ backgroundColor: "#82aefe" });
+    });
+
+    $("#green").on("click", function () {
+        $("body").css({ backgroundColor: "#b2ffb4" });
+    });
+});
